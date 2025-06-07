b5af56ca119aa62f7bfbd5242f1053cebe6f243a
Neil Smyth <neil@thesmyths.eu>
2025-06-07 07:35:26 +0200

    fixed script execution

--- transform/src/test-geocode.ts
+++ transform/src/test-geocode.ts
@@ -8,7 +8,7 @@ const main = async () => {
 
   logger.info('Testing out Geocode api');
   const apiKey = '4cfbe072a6904698aa21382c71a3a44c'
-  const geocodeHandler = new GeoapifyGeocodeHandler(apiKey);
+  const geocodeHandler = new GeoapifyGeocodeHandler(apiKey, logger);
   const resuolt = await geocodeHandler.lookup('IE', '', 'test');
 };
 
